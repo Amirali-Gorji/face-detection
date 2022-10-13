fac6134e45a3130b809096475ad49180a6ee8579
amirali <amirali.gorji@gmail.com>
2022-10-13 21:49:08 +0330

    add some comments and messages

--- face_detection.py
+++ face_detection.py
@@ -1,24 +1,27 @@
 import cv2 as cv
 
-path = r'D:\programming\Face detection\src\pictures\train\hajghasem\19.jpg'
+path = r'./assets/picture/samplePicture.jpg'
 
 img = cv.imread(path)
 
 #print (img)
+# make picture gray
 grayPicture = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 
 #print(grayPicture)
 
 #cv.imshow('picutre',grayPicture)
 
+# set classifier 
 haar_face_classifier = cv.CascadeClassifier('haar_face.xml')
 
+# use classifier to detect faces 
 faces_rect = haar_face_classifier.detectMultiScale(grayPicture , scaleFactor=1.04 , minNeighbors=3)
 
-#print(f'number is {len(faces_rect)}')
 
+print("Coordinates of faces:\n")
 print(faces_rect)
-
+print("Wait for picture to popup...")
 
 for (x,y,w,h) in faces_rect:
     cv.rectangle(img , (x,y) , (x+h,y+w) , (255,0,0) , thickness=1)
